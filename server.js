007f165dbf009c2378e15a68b0167363c089f696
YURI GABRIEL SILVA DE JESUS <yuri.gsjesus@senacsp.edu.br>
2025-07-08 13:53:12 -0300

    atualizações da API

--- server.js
+++ server.js
@@ -37,15 +37,39 @@ app.delete("/tarefas/:id", async (req, res) => {
   }
 });
 
-app.get("/tarefas/id", async (req, res) => {
+app.get("/tarefas/:id", async (req, res) => {
   try {
+    const { id } = req.params;
     const tarefas = await serVer.findById(id);
     if (!tarefas) {
-      return res.status(400).json({ erro: "Tarefa não Encontrada" });
+      return res.status(404).json({ erro: "Tarefa não Encontrada" }); // 404 é mais apropriado para "não encontrado"
     }
-    res.status(219).json(tarefas);
+    res.status(200).json(tarefas);
   } catch (error) {
-    res.status(500).json({ erro: "Erro ao Remover Tarefa" });
+    res.status(500).json({ erro: "Erro ao Buscar Tarefa" });
+  }
+});
+
+app.put("/tarefas/:id", async (req, res) => {
+  const { id } = req.params;
+  const { descricao } = req.body;
+  if (!descricao) {
+    return res.status(400).json({ erro: "Preencha a nova descrição" });
+  }
+  try {
+    const tarefas = await serVer.findByIdAndUpdate(
+      id,
+      { descricao },
+      { new: true }
+    );
+
+    if (!tarefas) {
+      return res.status(404).json({ erro: "Tarefa não existe" });
+    }
+    res.json(tarefas);
+  } catch {
+    error;
+    res.status(500).json({ erro: "Erro ao atualizar tarefa" });
   }
 });
 
